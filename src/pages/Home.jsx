9f838cff56802d03eee6e73be2891f38a5844f25
hxxtxrchq <calonsoparedes1@gmail.com>
2026-03-11 10:54:52 -0500

    Restore home initial loading screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 import Hero from '../components/home/Hero';
 import ServicesCarousel from '../components/home/ServicesCarousel';
 import PortfolioSplitHero from '../components/home/PortfolioSplitHero';
+import InitialLoading from '../components/InitialLoading';
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(() => {
+    if (typeof window === 'undefined') return false;
+    return sessionStorage.getItem('pb-home-loader-seen') !== '1';
+  });
+
+  const handleLoadingComplete = () => {
+    sessionStorage.setItem('pb-home-loader-seen', '1');
+    setIsLoading(false);
+  };
+
+  if (isLoading) {
+    return <InitialLoading onComplete={handleLoadingComplete} />;
+  }
+
   return (
     <motion.div
       initial={{ opacity: 1 }}
